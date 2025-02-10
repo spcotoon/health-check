1688df964e8af8ff446a5bab841f3d00a59cd924
spcotoon <springcomicskr@gmail.com>
2025-02-11 01:14:38 +0900

    add logger

--- src/main/java/com/example/demo/HealthController.java
+++ src/main/java/com/example/demo/HealthController.java
@@ -1,19 +1,23 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-public class Controller {
+public class HealthController {
 
     @GetMapping("/")
     public ResponseEntity<String> main() {
+        log.info("connected");
         return ResponseEntity.ok().body("welcome");
     }
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
+        log.info("healthy");
         return ResponseEntity.ok().body("i am healthy");
     }
 }
